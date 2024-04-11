fedfff75853c9fe5be71ddc361df4aaf5621363c
JELLOULI Youness <younessjellouli12@gmail.com>
2024-04-11 16:12:32 +0000

    Update import statement for @vercel/analytics package in src/index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
